457847f8a74f6981b8fb656ab04cd18c38468af1
Owidiusz Zieliński <zielinski.owidiusz@gmail.com>
2025-06-13 05:21:13 +0200

    Added auto load data

--- src/main/kotlin/org/example/ecoswapbackend/DataLoader.kt
+++ src/main/kotlin/org/example/ecoswapbackend/DataLoader.kt
@@ -1,10 +1,16 @@
 package org.example.ecoswapbackend
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.example.ecoswapbackend.item.model.Item
+import org.example.ecoswapbackend.item.model.ItemRepository
 import org.example.ecoswapbackend.user.User
 import org.example.ecoswapbackend.user.UserRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import java.math.BigDecimal
 
 @Configuration
 class DataLoader {
@@ -20,5 +26,15 @@ class DataLoader {
         )
 
         userRepository.saveAll(users)
+    }
+
+    @Bean
+    fun loadItems(itemRepository: ItemRepository) = CommandLineRunner {
+        if (itemRepository.count() == 0L) {
+            val mapper = jacksonObjectMapper()
+            val resource = ClassPathResource("ecoswap.items.json")
+            val items: List<Item> = mapper.readValue(resource.inputStream)
+            itemRepository.saveAll(items)
+        }
     }
 }
